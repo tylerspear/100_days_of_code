77993fcfb0d3de0b0e1966752cdc8487654f9ac9
tylerspear <stylerspear@gmail.com>
2023-04-05 22:25:10 -0500

    basic functionality for password generator

---
+++ day5_loops/password_generator.py
@@ -0,0 +1,30 @@
+import random
+num_letters = int(input("How many letters would you like in the password?: "))
+num_numbers = int(input("How many numbers would you like in the password?: "))
+num_symbols = int(input("How many symbols would you like in the password?: "))
+
+list_letter = ['A', 'B', 'C', 'D' 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+list_number = [1,2,3,4,5,6,7,8,9]
+list_symbol = ['!','@','#','$','%','&','*','(',')','-','+']
+
+final_pass = ''
+
+#letters
+for letter in range(0,num_letters):
+    is_lower = random.randint(0,1)
+    if is_lower == 0:
+        rand_letter = random.randint(0,len(list_letter))
+        final_pass += list_letter[rand_letter].lower()
+    else:
+        rand_letter = random.randint(0,len(list_letter))
+        final_pass += list_letter[rand_letter]
+#nums
+for num in range(0,num_numbers):
+    rand_number = random.randint(0,len(list_number))
+    final_pass += str(list_number[rand_number])
+#symbols
+for symb in range(0,num_symbols):
+    rand_symbol = random.randint(0,len(list_symbol))
+    final_pass += list_symbol[rand_symbol]
+
+print(final_pass)
