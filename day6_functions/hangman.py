e965b64752c9b54158bf12a7e2361c03e8537d14
tylerspear <stylerspear@gmail.com>
2023-04-10 22:03:08 -0500

    base functionality of hangman game

--- day6_functions/hangman.py
+++ day6_functions/hangman.py
@@ -1,19 +1,49 @@
 import random
+# from words import words
+from art import stages
+
+
+guesses = len(stages) - 1
+
 word_list = ["ardvark", "baboon", "camel"]
 
 #randomly choose a word from the list
 chosen_word = word_list[random.randint(0, len(word_list) - 1)]
 
-#ask for user to guess a letter
-player_guess = input("Guess a letter: ").lower()
-
 display = []
 
 for char in chosen_word:
-    display.append('_')
+        display.append('_')
 
-for i in range(0, len(chosen_word)-1):
-    if chosen_word[i] == player_guess:
-        display[i] = player_guess       
-print(display)
+game_over = False
+
+while not game_over:
+    
+    player_guess = input("Guess a letter: ").lower()
+
+    if player_guess not in chosen_word:
+         print(stages[guesses])
+         guesses -= 1
+         
+         if guesses == -1:
+            print("You lose! Better luck next time")
+            print((f'The word was {chosen_word}'))
+            game_over = True
+            break 
+
+    for i in range(0, len(chosen_word)):
+        if chosen_word[i] == player_guess:
+            display[i] = player_guess
+        
+    # if the 2 words match, you win
+    if ''.join(display) == chosen_word:
+        print("Winner!")
+        game_over = True
+        print(''.join(display))
+        break
+
+    #other wise keep going
+    else:
+        print(display)
+    
 
