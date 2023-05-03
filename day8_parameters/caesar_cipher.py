b6754bfeea0b92b88ee4072896be93d0f3f1bc12
tylerspear <stylerspear@gmail.com>
2023-05-02 22:06:19 -0500

    completed caesar cipher

--- day8_parameters/caesar_cipher.py
+++ day8_parameters/caesar_cipher.py
@@ -6,7 +6,26 @@ def encrypt():
     shift = int(input("Enter a number to shift by: "))
     for char in str:
         encoded.append(alph[(alph.index(char) + shift)%26])
+    print("The coded message is:")
     print(''.join(encoded))
 
+def decrypt():
+    decoded = []
+    str = input("What message would you like to decrypt?: ").lower()
+    shift = int(input("What is the decryption number?: "))
+    for char in str:
+        decoded.append(alph[(alph.index(char) - shift)%26])
+    print("The decoded message is:")
+    print(''.join(decoded))
 
-encrypt()
+user_choice = input("Would you like to encrypt or decrypt a message?: ")
+
+if user_choice == "encrypt":
+    encrypt()
+elif user_choice == "decrypt":
+    decrypt()
+else:
+    "Sorry, I don't recognize that command"
+
+
+
