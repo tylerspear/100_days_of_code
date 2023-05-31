a2b7d359aaf6fc09d42fb1ce4b79e493d89c8737
tylerspear <stylerspear@gmail.com>
2023-05-30 22:04:24 -0500

    added player/dealer functionality and bust check

--- day11_blackjack/blackjack.py
+++ day11_blackjack/blackjack.py
@@ -1,4 +1,5 @@
 from random import randrange
+
 def draw(deck, num_cards):
     cards = []
     for i in range(num_cards):
@@ -10,12 +11,26 @@ def blackjack():
     deck = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
     # random 2 cards to dealer and player
     dealer = draw(deck, 2)
-    print(dealer)
-    # check for blackjack (Ace + 10)
+    player = draw(deck, 2)
+    dealer_total = sum(dealer)
+    player_total = sum(player)
+    if dealer_total == 21:
+        print('Blackjack! Dealer wins')
+    else:
+        if player_total == 21:
+            print('Blackjack! Player wins')
+        while player_total < 21:
+            print(f"Dealer's 1st card: {dealer[0]}")
+            print(f"Your Cards: {player}, Total: {player_total}")
+            hit = input('Would you like another card? (y/n): ')
+            if hit == 'y':
+                player.append(draw(deck, 1)[0])
+                player_total = sum(player)
+        if player_total > 21:
+            print(f"Your Cards: {player}, Total: {player_total}")
+            print('Bust!')
 
-    # if both have blackjack, pc wins
-
-    # if only 1 has blackjack then they win
+        
 
 
 
